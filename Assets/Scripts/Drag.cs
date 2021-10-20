1688dccb9fb5e925f69895087ec4e37770db4da4
Santiagospinai7 <santiagospidrobo15@gmail.com>
2021-10-19 21:53:38 -0500

    se creo el segundo nivel y el sistemas de puntuacion con estrelals

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -15,8 +15,13 @@ public class Drag : MonoBehaviour
     private float gameObjectPositionY;
     private float gameObjectPositionZ;
 
+    private GameObject parent;
+    private Levels levels;
+
     private void Start()
     {
+        parent = GameObject.Find("Parent");
+        levels = parent.GetComponent<Levels>();
         floor = GameObject.Find("Floor");
         joinAudio = floor.GetComponent<AudioSource>();
     }
@@ -65,65 +70,121 @@ public class Drag : MonoBehaviour
 
     void OnMouseDrag()
     {
-        //isMoving = true;
-
         float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
         transform.position = new Vector3(pos_move.x, transform.position.y, pos_move.z);
     }
 
-    private void OnMouseExit()
-    {
-        //isMoving = false;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        //GREEN
-        if (sphere.gameObject.tag == "blue" && other.gameObject.tag == "yellow" || sphere.gameObject.tag == "yellow" && other.gameObject.tag == "blue")
+        if(MenuScript.greenLevel)
         {
-            foreach (var x in materials) {
-                if (x.name == "green") {
-                    sphere.GetComponent<Renderer>().material = x;
-                    sphere.gameObject.tag = "green";
-                    Debug.Log("Convertir en verde.");
-                }
-            }
-            joinAudio.Play(0);
-            StartCoroutine(tweenCoroutine());
-            Destroy(other.gameObject);
-        }
-        //ORANGE
-        if (sphere.gameObject.tag == "red" && other.gameObject.tag == "yellow" || sphere.gameObject.tag == "yellow" && other.gameObject.tag == "red")
-        {
-            foreach (var x in materials)
+            //GREEN
+            if (sphere.gameObject.tag == "blue" && other.gameObject.tag == "yellow" || sphere.gameObject.tag == "yellow" && other.gameObject.tag == "blue")
             {
-                if (x.name == "orange")
+                foreach (var x in materials)
                 {
-                    sphere.GetComponent<Renderer>().material = x;
-                    sphere.gameObject.tag = "orange";
-                    Debug.Log("Convertir en naranja.");
+                    if (x.name == "green")
+                    {
+                        sphere.GetComponent<Renderer>().material = x;
+                        sphere.gameObject.tag = "green";
+                        Debug.Log("Convertir en verde.");
+                    }
                 }
+                joinAudio.Play(0);
+                StartCoroutine(tweenCoroutine());
+                levels.completeGoal = true;
+                Destroy(other.gameObject);
             }
-            joinAudio.Play(0);
-            StartCoroutine(tweenCoroutine());
-            Destroy(other.gameObject);
-        }
-        //VIOLET
-        if (sphere.gameObject.tag == "blue" && other.gameObject.tag == "red" || sphere.gameObject.tag == "red" && other.gameObject.tag == "blue")
-        {
-            foreach (var x in materials)
+            //ORANGE
+            if (sphere.gameObject.tag == "red" && other.gameObject.tag == "yellow" || sphere.gameObject.tag == "yellow" && other.gameObject.tag == "red")
             {
-                if (x.name == "violet")
+                foreach (var x in materials)
                 {
-                    sphere.GetComponent<Renderer>().material = x;
-                    sphere.gameObject.tag = "violet";
-                    Debug.Log("Convertir en morado.");
+                    if (x.name == "orange")
+                    {
+                        sphere.GetComponent<Renderer>().material = x;
+                        sphere.gameObject.tag = "orange";
+                        Debug.Log("Convertir en naranja.");
+                    }
                 }
+                joinAudio.Play(0);
+                StartCoroutine(tweenCoroutine());
+                levels.error = true;
+                Destroy(other.gameObject);
             }
-            joinAudio.Play(0);
-            StartCoroutine(tweenCoroutine());
-            Destroy(other.gameObject);
+            //VIOLET
+            if (sphere.gameObject.tag == "blue" && other.gameObject.tag == "red" || sphere.gameObject.tag == "red" && other.gameObject.tag == "blue")
+            {
+                foreach (var x in materials)
+                {
+                    if (x.name == "violet")
+                    {
+                        sphere.GetComponent<Renderer>().material = x;
+                        sphere.gameObject.tag = "violet";
+                        Debug.Log("Convertir en morado.");
+                    }
+                }
+                joinAudio.Play(0);
+                StartCoroutine(tweenCoroutine());
+                levels.error = true;
+                Destroy(other.gameObject);
+            }
+        }
+
+        if (MenuScript.violetLevel)
+        {
+            //GREEN
+            if (sphere.gameObject.tag == "blue" && other.gameObject.tag == "yellow" || sphere.gameObject.tag == "yellow" && other.gameObject.tag == "blue")
+            {
+                foreach (var x in materials)
+                {
+                    if (x.name == "green")
+                    {
+                        sphere.GetComponent<Renderer>().material = x;
+                        sphere.gameObject.tag = "green";
+                        Debug.Log("Convertir en verde.");
+                    }
+                }
+                joinAudio.Play(0);
+                StartCoroutine(tweenCoroutine());
+                levels.error = true;
+                Destroy(other.gameObject);
+            }
+            //ORANGE
+            if (sphere.gameObject.tag == "red" && other.gameObject.tag == "yellow" || sphere.gameObject.tag == "yellow" && other.gameObject.tag == "red")
+            {
+                foreach (var x in materials)
+                {
+                    if (x.name == "orange")
+                    {
+                        sphere.GetComponent<Renderer>().material = x;
+                        sphere.gameObject.tag = "orange";
+                        Debug.Log("Convertir en naranja.");
+                    }
+                }
+                joinAudio.Play(0);
+                StartCoroutine(tweenCoroutine());
+                levels.error = true;
+                Destroy(other.gameObject);
+            }
+            //VIOLET
+            if (sphere.gameObject.tag == "blue" && other.gameObject.tag == "red" || sphere.gameObject.tag == "red" && other.gameObject.tag == "blue")
+            {
+                foreach (var x in materials)
+                {
+                    if (x.name == "violet")
+                    {
+                        sphere.GetComponent<Renderer>().material = x;
+                        sphere.gameObject.tag = "violet";
+                        Debug.Log("Convertir en morado.");
+                    }
+                }
+                joinAudio.Play(0);
+                StartCoroutine(tweenCoroutine());
+                levels.completeGoal = true;
+                Destroy(other.gameObject);
+            }
         }
     }
 
@@ -133,6 +194,7 @@ public class Drag : MonoBehaviour
         LeanTween.scale(sphere, new Vector3(0.85f, 0.85f, 0.85f), 0.1f);
         yield return new WaitForSeconds(0.1f);
         LeanTween.scale(sphere, new Vector3(1.2f, 1.2f, 1.2f), 0.1f);
+        
         yield return new WaitForSeconds(0.1f);
         LeanTween.scale(sphere, new Vector3(0.9f, 0.9f, 0.9f), 0.1f);
         yield return new WaitForSeconds(0.1f);
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,10 +2,111 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
+    public static bool greenLevel;
+    public static int greenLevelStars;
+
+    public static bool violetLevel;
+    public static int violetLevelStars;
+
+    public static bool orangeLevel;
+    public static int orangeLevelStars;
+
+    public GameObject[] greenStars;
+    public GameObject[] violetStars;
+
+    public Sprite goldStar;
+    public Sprite whiteStar;
+
+    private void Start()
+    {
+        //if (greenLevel) {
+            resetGreenStars();
+            switch (greenLevelStars)
+            {
+                case 1:
+                    for (var i = 0; i < (greenStars.Length - 2); i++)
+                    {
+                        greenStars[i].GetComponent<Image>().sprite = goldStar;
+                    }
+                    break;
+                case 2:
+                    for (var y = 0; y < (greenStars.Length - 1); y++)
+                    {
+                        greenStars[y].GetComponent<Image>().sprite = goldStar;
+                    }
+                    break;
+                case 3:
+                    for (var x = 0; x < greenStars.Length; x++)
+                    {
+                        greenStars[x].GetComponent<Image>().sprite = goldStar;
+                    }
+                    break;
+            }
+        //}
+
+        //if (violetLevel) {
+            resetVioletStars();
+            switch (violetLevelStars)
+            {
+                case 1:
+                    for (var i = 0; i < (greenStars.Length - 2); i++)
+                    {
+                        violetStars[i].GetComponent<Image>().sprite = goldStar;
+                    }
+                    break;
+                case 2:
+                    for (var y = 0; y < (greenStars.Length - 1); y++)
+                    {
+                        violetStars[y].GetComponent<Image>().sprite = goldStar;
+                    }
+                    break;
+                case 3:
+                    for (var x = 0; x < greenStars.Length; x++)
+                    {
+                        violetStars[x].GetComponent<Image>().sprite = goldStar;
+                    }
+                    break;
+            }
+        //}
+        
+        resetBooleanLevels();
+    }
+
+    public void resetBooleanLevels() {
+        greenLevel = false;
+        violetLevel = false;
+        orangeLevel = false;
+    }
+
+    void resetGreenStars() {
+        Debug.Log("test1  reseteo las verdes");
+        for (var x = 0; x < greenStars.Length; x++)
+        {
+            //greenStars[x].GetComponent<Image>().sprite = whiteStar;
+        }
+    }
+
+    void resetVioletStars()
+    {
+        Debug.Log("test1  reseteo las violetas");
+        for (var x = 0; x < violetStars.Length; x++)
+        {
+            //violetStars[x].GetComponent<Image>().sprite = whiteStar;
+        }
+    }
+
     public void firstLevel() {
+        greenLevel = true;
         SceneManager.LoadScene("First_Level");
+    }
+
+    public void secondLevel()
+    {
+        violetLevel = true;
+        SceneManager.LoadScene("Second_Level");
     }
 }
